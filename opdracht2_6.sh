b2b40866c9feb9b5a818b76fadac1e010481775a
Wilciway <39240157+Wilciway@users.noreply.github.com>
2019-04-05 23:36:08 +0200

    Create opdracht2_6.sh

---
+++ opdracht2_6.sh
@@ -0,0 +1,12 @@
+#print willekeurig de letters van het meegegeven woord door (bijv. met ./opdracht2_6.sh woord)
+string=$1
+stringLength=$(echo $string | wc -m)
+
+RANDOM=$$
+while :; do
+        randomNum=$(( $RANDOM % $stringLength ))
+        randomChar=${1:$randomNum:1}
+        printf "%*s" $randomNum $randomChar
+        sleep 0.01
+done
+
